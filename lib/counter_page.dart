76aa8553d998a0c9f74c732db6a71e7ec206250e
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-03-21 05:38:33 +0700

    digunakan untuk public dan private/local

--- lib/counter_page.dart
+++ lib/counter_page.dart
@@ -1,1 +1,21 @@
-odo
+import 'package:flutter/material.dart';
+
+// Digunakan untuk public
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+// Digunakan untuk private/local
+class _CounterPageState extends State<CounterPage> {
+  List<String> listCounter = [];
+  int _counter = 1;
+
+  void addData() {
+    setState(() {
+      _counter += 1;
+      listCounter.add(_counter.toString());
+    });
+  }
