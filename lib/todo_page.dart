1aed6b660542fb0c330db4996277a06fad59ab0d
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-03-21 05:46:03 +0700

    Tambah daftar tugas TodoPage

--- lib/todo_page.dart
+++ lib/todo_page.dart
@@ -16,4 +16,15 @@ class _TodoPageState extends State<TodoPage> {
   bool _isError = false;
   bool _isDateError = false;
 
- 
+  List<Map<String, dynamic>> tasks = [
+    {
+      'title': 'Tugas PAM Lanjut',
+      'deadline': DateTime(2025, 3, 15, 14, 3),
+      'done': false,
+    },
+    {
+      'title': 'Balik Kampung',
+      'deadline': DateTime(2025, 3, 14, 12, 3),
+      'done': true,
+    },
+  ];
