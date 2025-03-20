1fc1e2e51b548a9cbea56202e207946ed3df4f71
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2025-03-21 05:54:08 +0700

    Memperbaiki error widget_test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pertemuantiga/main.dart';
+import 'package:pertemuan_tiga/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
